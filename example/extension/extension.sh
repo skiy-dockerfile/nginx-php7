bebe39cd95166505d782ee3231761f5e53239658
skiy <skiychan@outlook.com>
2021-07-20 19:11:45 +0800

    Add docker-compose.yaml

--- example/extension/extension.sh
+++ example/extension/extension.sh
@@ -2,8 +2,8 @@
 
 mkdir /home/extension
 
-curl -Lk https://github.com/swoole/swoole-src/archive/v4.4.14.tar.gz | gunzip | tar x -C /home/extension && \
-cd /home/extension/swoole-src-4.4.14 && \
+curl -Lk https://github.com/swoole/swoole-src/archive/v4.7.0.tar.gz | gunzip | tar x -C /home/extension && \
+cd /home/extension/swoole-src-4.7.0 && \
 /usr/local/php/bin/phpize && \
 ./configure --with-php-config=/usr/local/php/bin/php-config && \
 make && make install
